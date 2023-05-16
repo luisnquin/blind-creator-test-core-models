a2981d847607198f60d7ec4892feb1e1ebd95b61
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-05-15 21:55:35 -0600

    fix(migrations): as part of library migration, foreign keys are temporary removed

--- migrations/postgres.go
+++ migrations/postgres.go
@@ -1,7 +1,9 @@
 package migrations
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/luisnquin/blind-creator-test-core-models/models/agencies"
 	"github.com/luisnquin/blind-creator-test-core-models/models/campaign_creator_social_network_actions"
@@ -12,14 +14,30 @@ import (
 	"github.com/luisnquin/blind-creator-test-core-models/models/user_agency_relations"
 	"github.com/luisnquin/blind-creator-test-core-models/models/users"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
+
+type SilentLogger struct{}
+
+func (l SilentLogger) LogMode(logger.LogLevel) logger.Interface {
+	return l
+}
+
+func (l SilentLogger) Info(context.Context, string, ...interface{}) {}
+
+func (l SilentLogger) Warn(context.Context, string, ...interface{}) {}
+
+func (l SilentLogger) Error(context.Context, string, ...interface{}) {}
+
+func (l SilentLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+}
 
 func ApplyMigrations(dbConnection *gorm.DB) {
 	// if !applyMigrations {
 	// 	return
 	// }
 
-	dbConnection.LogMode(true)
+	dbConnection.Logger = SilentLogger{} // Silent mode for now
 
 	// MODELS
 	log.Println("users...")
@@ -48,29 +66,31 @@ func ApplyMigrations(dbConnection *gorm.DB) {
 
 	// RELATIONS
 
+	// TODO: add foreign keys using model tags
+
 	log.Println("user_agency_relations...")
-	dbConnection.Model(&user_agency_relations.UserAgencyRelation{}).
-		AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE").
-		AddForeignKey("agency_id", "agencies(id)", "CASCADE", "CASCADE")
+	dbConnection.Model(&user_agency_relations.UserAgencyRelation{}) //.
+	// AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE").
+	// AddForeignKey("agency_id", "agencies(id)", "CASCADE", "CASCADE")
 
 	log.Println("creator_social_networks...")
-	dbConnection.Model(&creator_social_networks.CreatorSocialNetworkAccount{}).
-		AddForeignKey("creator_id", "creators(id)", "CASCADE", "CASCADE")
+	dbConnection.Model(&creator_social_networks.CreatorSocialNetworkAccount{}) //.
+	// AddForeignKey("creator_id", "creators(id)", "CASCADE", "CASCADE")
 
 	log.Println("companies...")
-	dbConnection.Model(&companies.Company{}).
-		AddForeignKey("agency_id", "agencies(id)", "CASCADE", "CASCADE").
-		AddForeignKey("manager_id", "users(id)", "CASCADE", "CASCADE")
+	dbConnection.Model(&companies.Company{}) //.
+	// AddForeignKey("agency_id", "agencies(id)", "CASCADE", "CASCADE").
+	// AddForeignKey("manager_id", "users(id)", "CASCADE", "CASCADE")
 
 	log.Println("campaigns...")
-	dbConnection.Model(&campaigns.Campaign{}).
-		AddForeignKey("agency_id", "agencies(id)", "CASCADE", "CASCADE").
-		AddForeignKey("manager_id", "users(id)", "CASCADE", "CASCADE").
-		AddForeignKey("company_id", "companies(id)", "CASCADE", "CASCADE")
+	dbConnection.Model(&campaigns.Campaign{}) //.
+	// AddForeignKey("agency_id", "agencies(id)", "CASCADE", "CASCADE").
+	// AddForeignKey("manager_id", "users(id)", "CASCADE", "CASCADE").
+	// AddForeignKey("company_id", "companies(id)", "CASCADE", "CASCADE")
 
 	log.Println("campaign_creator_social_network_actions ...")
-	dbConnection.Model(&campaign_creator_social_network_actions.CampaignCreatorSocialNetworkActions{}).
-		AddForeignKey("campaign_id", "campaigns(id)", "CASCADE", "CASCADE").
-		AddForeignKey("creator_id", "creators(id)", "CASCADE", "CASCADE").
-		AddForeignKey("creator_social_network_id", "creator_social_networks(id)", "CASCADE", "CASCADE")
+	dbConnection.Model(&campaign_creator_social_network_actions.CampaignCreatorSocialNetworkActions{}) //.
+	// AddForeignKey("campaign_id", "campaigns(id)", "CASCADE", "CASCADE").
+	// AddForeignKey("creator_id", "creators(id)", "CASCADE", "CASCADE").
+	// AddForeignKey("creator_social_network_id", "creator_social_networks(id)", "CASCADE", "CASCADE")
 }
